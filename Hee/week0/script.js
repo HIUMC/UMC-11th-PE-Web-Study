fdfb7c65eaef956b3bfd5ff578a8942bbe8918b7
remmanics <foseja1114@gmail.com>
2026-09-23 09:41:45 +0900

    2주차 미션 완료

--- Hee/week0/script.js
+++
@@ -1,6 +0,0 @@
-const message = document.querySelector("#message");
-const cheerButton = document.querySelector("#cheer-button");
-
-cheerButton.addEventListener("click", function () {
-  message.textContent = "화이팅입니다에요. 🚀";
-});
--- yoni/week0/script.js
+++
@@ -1,6 +0,0 @@
-const message = document.querySelector("#message");
-const cheerButton = document.querySelector("#cheer-button");
-
-cheerButton.addEventListener("click", function(){
-    message.textContent = "좋아요! 굿굿";
-});
